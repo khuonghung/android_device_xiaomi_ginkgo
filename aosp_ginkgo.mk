9953ddd9f77db4ea0b116a9ee5b812cccab1ac07
Khuong Hung <46420538+khuonghung1423@users.noreply.github.com>
2021-08-15 03:10:33 +0700

    ginkgo: drop ANXCamera

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -12,7 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
-$(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Inherit some common PixelExtended stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
